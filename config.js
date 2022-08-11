d9a6eac74811416e0ba93c86618d77297f60e87f
yiyayiyayiyayiya <111064094+yiyayiyayiyayiya@users.noreply.github.com>
2022-08-11 21:54:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+// 艺呀艺呀艺 2022年8月12日 https://github.com/AJLoveChina/birthday
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
